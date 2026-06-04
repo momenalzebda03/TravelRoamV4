979df164e14b70e9b395034404d2e6e98a225374
Momen Alzebda <Momen07996@gmail.com>
2026-06-04 17:01:19 +0300

    update esim details

--- assets/js/qr.js
+++ assets/js/qr.js
@@ -38,31 +38,3 @@ function closeModal() {
 
 if (cancelBooking) cancelBooking.onclick = openModal;
 if (noModel) noModel.onclick = closeModal;
-
-// new QRCode(document.getElementById("qrcode"), {
-//     text: "https://example.com",
-//     width: 280,
-//     height: 280
-// });
-
-// var modelQr = document.getElementById("modelQr");
-// var cancelBooking = document.getElementById("cancelBooking");
-// var noModel = document.getElementById("noModel");
-
-// function openModal() {
-//     modelQr.classList.remove("hidden");
-//     modelQr.classList.add("flex");
-// }
-
-// function closeModal() {
-//     modelQr.classList.add("hidden");
-//     modelQr.classList.remove("flex");
-// }
-
-// if (cancelBooking) {
-//     cancelBooking.onclick = openModal;
-// }
-
-// if (noModel) {
-//     noModel.onclick = closeModal;
-// }
