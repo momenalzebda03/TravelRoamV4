258e5ce61858c0e17bf3484d811b30804caca0f3
Momen Alzebda <Momen07996@gmail.com>
2026-06-29 16:39:09 +0300

    update code lists

--- assets/js/landing-page.js
+++ assets/js/landing-page.js
@@ -1,15 +1,21 @@
 function showTab(tab) {
-    document.getElementById('list-country').classList.toggle('hidden', tab !== 'country')
-    document.getElementById('list-region').classList.toggle('hidden', tab !== 'region')
+    document.getElementById('list-country')
+        .classList.toggle('hidden', tab !== 'country');
 
-    document.getElementById('tab-country').classList.toggle('active-destinations', tab === 'country')
-    document.getElementById('tab-region').classList.toggle('active-destinations', tab === 'region')
+    document.getElementById('list-region')
+        .classList.toggle('hidden', tab !== 'region');
 
-    document.getElementById('btn-list-country').classList.remove('hidden');
-    document.getElementById('btn-list-region').classList.add('hidden');
+    document.getElementById('tab-country')
+        .classList.toggle('active-destinations', tab === 'country');
 
-    document.getElementById('btn-list-region').classList.remove('hidden');
-    document.getElementById('btn-list-country').classList.add('hidden');
+    document.getElementById('tab-region')
+        .classList.toggle('active-destinations', tab === 'region');
+
+    document.getElementById('btn-list-country')
+        .classList.toggle('hidden', tab !== 'country');
+
+    document.getElementById('btn-list-region')
+        .classList.toggle('hidden', tab !== 'region');
 }
 
 function selectDestination(name, code) {
